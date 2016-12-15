dcb4db06144477337e8121c6aa1b41e51a1cba3e
karapuz <karapuz@yandex-team.ru>
2016-12-15 17:00:45 +0300

    packages update

--- ya.sys.install.sh
+++ ya.sys.install.sh
@@ -4,7 +4,7 @@
 # Main project 
 #
 # sudo apt-get update
-sudo apt-get -y install tmux gcc g++ cmake emacs valgrind gdb gdbserver wireshark google-perftools aptitude pkg-config python python3 libboost-dev gnome-menus desktop-file-utils bamfdaemon mime-support git liberror-perl git-man curl wget perl
+sudo apt-get -y install tmux gcc g++ cmake emacs valgrind gdb gdbserver wireshark google-perftools aptitude pkg-config python python3 libboost-dev gnome-menus desktop-file-utils bamfdaemon mime-support git liberror-perl git-man curl wget perl locate setuptools doxygen htop openssh python-yaml 
 
 wget -c https://atom.io/download/deb -O ~/Download/atom-amd64.install.deb
 sudo dpkg -i ~/Download/atom-amd64.install.deb
@@ -22,4 +22,5 @@ git clone https://github.com/cocaine/cocaine-native-proxy
 git clone https://github.com/cocaine/cocaine-flow
 git clone https://github.com/cocaine/cocaine-fs
 popd
+
 
