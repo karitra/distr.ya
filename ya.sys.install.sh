2d0b61ea69a1d0e934c7dff9493e01e2b6479b06
karapuz <karapuz@yandex-team.ru>
2016-12-15 19:15:45 +0300

    tmux config

--- ya.sys.install.sh
+++ ya.sys.install.sh
@@ -4,7 +4,9 @@
 # Main project 
 #
 # sudo apt-get update
-sudo apt-get -y install tmux gcc g++ cmake emacs valgrind gdb gdbserver wireshark google-perftools aptitude pkg-config python python3 libboost-dev gnome-menus desktop-file-utils bamfdaemon mime-support git liberror-perl git-man curl wget perl locate setuptools doxygen htop python-yaml  unity-tweak-tool 
+sudo apt-get -y install gcc g++ cmake emacs valgrind gdb gdbserver wireshark google-perftools aptitude pkg-config python python3 libboost-dev gnome-menus desktop-file-utils bamfdaemon mime-support git liberror-perl git-man curl wget perl locate setuptools doxygen htop python-yaml  unity-tweak-tool 
+sudo apt-get -y install tmux
+
 
 ATOMPKG=~/Downloads/atom-amd64.install.deb
 
