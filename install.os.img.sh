1bc252e093dd649ce918ad8d2305a5934d85ae16
karapuz <karapuz@yandex-team.ru>
2016-12-15 19:42:59 +0300

    ostack script

--- install.os.img.sh
+++ install.os.img.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 virt-install --virt-type kvm --name trusty --ram 2048 --cdrom=/home/karapuz/Downloads/mini.iso   --disk /tmp/trusty.qcow2,format=qcow2  --network network=default   --graphics vnc,listen=0.0.0.0 --noautoconsole    --os-type=linux --os-variant=ubuntutrusty
+
